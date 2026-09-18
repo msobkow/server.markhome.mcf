adf9088f40748ea76712b1cd95508492da2aff63
Mark Sobkow <mark.sobkow@gmail.com>
2026-09-17 19:05:30 -0600

    Blah

--- src/main/java/server/markhome/mcf/IMCFLibEntry.java
+++ src/main/java/server/markhome/mcf/IMCFLibEntry.java
@@ -1,4 +1,23 @@
-package server.markhome.mcf.v3_1.cflib;
+/*
+ * Copyright 2026 Mark Stephen Sobkow (mark.sobkow@gmail.com)
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://apache.org
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
+package server.markhome.mcf;
+
 
 public interface IMCFLibEntry {
 
@@ -23,7 +42,7 @@ public interface IMCFLibEntry {
 	 *
 	 *	Implementations should define a public static final constant string prefixed by the uppercase library name matching the following signature:
 	 *
-	 *	<tt>public static final String MCF_LIB_NAME = LIB_PARENT_NAME + ".mcf";</tt>
+	 *	<tt>public static final String MCF_LIB_NAME = MCF_LIB_PARENT_NAME + ".mcf";</tt>
 	 */
 
 	/**
@@ -31,13 +50,13 @@ public interface IMCFLibEntry {
 	 *
 	 *	Implementations should define a public static final constant string prefixed by the uppercase library name matching the following signature:
 	 *
-	 *	<tt>public static final String MCF_LIB_VERSION = LIB_PARENT_VERSION;</tt>
+	 *	<tt>public static final String MCF_LIB_VERSION = MCF_LIB_PARENT_VERSION;</tt>
 	 */
 
 	/**
 	 *	Implementations must be a singleton, returning the same instance over and over after initialization. How this is achieved may vary.
 	 *
-	 *	<tt>protected final static singleton = new AtomicReference<MCFLibraryEntry>(null);</tt>
+	 *	<tt>protected final static singleton = new AtomicReference<MCFLibEntry>(null);</tt>
 	 */
 
 	/**
@@ -61,7 +80,7 @@ public interface IMCFLibEntry {
 	 *
 	 *	Implementations should define a public static final method with the following signature, using the uppercase package name before "Lib" as appropriate.
 	 *
-	 *	<tt>public final static MCFLibraryEntry getMCFLibEntryInstance()</tt>
+	 *	<tt>public final static MCFLibEntry getMCFLibEntryInstance()</tt>
 	 *
 	 *	@return The instance, if any. Always returns null.
 	 */
--- src/main/java/server/markhome/mcf/MCFLibEntry.java
+++ src/main/java/server/markhome/mcf/MCFLibEntry.java
@@ -1,4 +1,22 @@
-package server.markhome.mcf.v3_1.cflib;
+/*
+ * Copyright 2026 Mark Stephen Sobkow (mark.sobkow@gmail.com)
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://apache.org
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
+package server.markhome.mcf;
 
 import java.util.concurrent.atomic.AtomicReference;
 
@@ -11,48 +29,48 @@ public final class MCFLibEntry implements IMCFLibEntry {
 	 *
 	 *	<tt>public static final String MCF_LIB_PARENT_NAME = "server.markhome";</tt>
 	 */
-	public static final String LIB_PARENT_NAME = "server.markhome";
+	public static final String MCF_LIB_PARENT_NAME = "server.markhome";
 
 	/**
 	 *	The parent resource name for this library.
 	 *
 	 *	Implementations should define a public static final constant string prefixed by the uppercase library name matching the following signature:
 	 *
-	 *	<tt>public static final String MCF_LIB_PARENT_VERSION = "3.1.42-2026091111";</tt>
+	 *	<tt>public static final String MCF_LIB_PARENT_VERSION = "3.1.42-20260918";</tt>
 	 */
-	public static final String LIB_PARENT_VERSION = "3.1.42-2026091111";
+	public static final String MCF_LIB_PARENT_VERSION = "3.1.42-20260918";
 
 	/**
 	 *	The public resource name for this library.
 	 *
 	 *	Implementations should define a public static final constant string prefixed by the uppercase library name matching the following signature:
 	 *
-	 *	<tt>public static final String MCF_LIB_NAME = LIB_PARENT_NAME + ".mcf";</tt>
+	 *	<tt>public static final String MCF_LIB_NAME = MCF_LIB_PARENT_NAME + ".mcf";</tt>
 	 */
-	public static final String LIB_NAME = LIB_PARENT_NAME + ".mcf";
+	public static final String MCF_LIB_NAME = MCF_LIB_PARENT_NAME + ".mcf";
 
 	/**
 	 *	The public version string for this library.
 	 *
 	 *	Implementations should define a public static final constant string prefixed by the uppercase library name matching the following signature:
 	 *
-	 *	<tt>public static final String MCF_LIB_VERSION = LIB_PARENT_VERSION;</tt>
+	 *	<tt>public static final String MCF_LIB_VERSION = MCF_LIB_PARENT_VERSION;</tt>
 	 */
-	public static final String LIB_VERSION = CFLIB_PARENT_VERSION;
+	public static final String MCF_LIB_VERSION = MCF_LIB_PARENT_VERSION;
 
 	/**
 	 *	Implementations must be a singleton, returning the same instance over and over after initialization. How this is achieved may vary.
 	 *
-	 *	<tt>protected final static singleton = new AtomicReference<MCFLibraryEntry>(null);</tt>
+	 *	<tt>protected final static singleton = new AtomicReference<MCFLibEntry>(null);</tt>
 	 */
-	protected final static singleton = new AtomicReference<MCFLibraryEntry>(null);
+	protected final static AtomicReference<MCFLibEntry> singleton = new AtomicReference<MCFLibEntry>(null);
 
 	/**
 	 *	Default constructor is protected so that only a singleton can be created.
 	 *
 	 *	<tt>protected MCFLibEntry { }</tt>
 	 */
-	protected MCFLibEntry { }
+	protected MCFLibEntry() { }
 
 	/**
 	 *	Get the singleton.
@@ -64,7 +82,7 @@ public final class MCFLibEntry implements IMCFLibEntry {
 	 *	@return Return the library singleton. Always returns the same instance after the first invocation unless reset(true) is invoked.
 	 */
 	public final static MCFLibEntry getMCFLibEntrySingleton() {
-		MCFLibraryEntry retval = singleton.get();
+		MCFLibEntry retval = singleton.get();
 		if( retval == null ) {
 			retval = new MCFLibEntry();
 			singleton.compareAndSet(null, retval);
@@ -78,11 +96,11 @@ public final class MCFLibEntry implements IMCFLibEntry {
 	 *
 	 *	Implementations should define a public static final method with the following signature, using the uppercase package name before "Lib" as appropriate.
 	 *
-	 *	<tt>public final static MCFLibraryEntry getMCFLibEntryInstance()</tt>
+	 *	<tt>public final static MCFLibEntry getMCFLibEntryInstance()</tt>
 	 *
 	 *	@return The instance, if any. Always returns null.
 	 */
-	public static MCFLibraryEntry getMCFLibEntryInstance() { return(null); }
+	public static MCFLibEntry getMCFLibEntryInstance() { return(null); }
 
 	/**
 	 *	Reset the singleton and any data caches after an application reload or other package or executable reloading event.
@@ -94,27 +112,22 @@ public final class MCFLibEntry implements IMCFLibEntry {
 	 *	@param	yesReally Are you sure you want to reset the value?
 	 */
 	public static boolean execMCFLibEntryLibReset(boolean yesReally) {
-		MCFLibraryEntry sgl = singleton.get();
+		MCFLibEntry sgl = singleton.get();
 		boolean retval;
-		if (sgl== null) {
+		if (sgl == null || yesReally) {
 			retval = false;
 
-			MCFLibraryEntry sgl = getSingleton();
-			assert(sgl != null, "Singleton must not be null after initialization");
+			if (yesReally && sgl != null) {
+				singleton.compareAndSet(sgl, null);
+			}
+
+			sgl = getMCFLibEntrySingleton();
+			assert sgl != null: "Singleton must not be null after initialization";
 
 			retval = true;
 		}
 		else {
 			retval = false;
-
-			if (yesReally) {
-				singleton.compareAndSet(retval, null);
-			}
-
-			MCFLibraryEntry sgl = getSingleton();
-			assert(sgl != null, "Singleton must not be null after initialization");
-
-			retval = true;
 		}
 		return (retval);
 	}
@@ -124,7 +137,7 @@ public final class MCFLibEntry implements IMCFLibEntry {
 	 *
 	 *	@return Return an instance if dynamically allocated, otherwise null. Because this is a singleton, null will always be returned.
 	 */
-	public MCFLibraryEntry getInstance() { return(getMCFLibEntryInstance()); }
+	public MCFLibEntry getInstance() { return(getMCFLibEntryInstance()); }
 
 	/**
 	 *	Get the singleton instance, if any. Invokes getMCFLibEntrySingleton() under the hood.
@@ -140,7 +153,7 @@ public final class MCFLibEntry implements IMCFLibEntry {
 	 *
 	 *	@return True if the library was reinitialized, false if the code detected that the library has not been used since the last reinitialization.
 	 */
-	public boolean libReset() { libReset(false); }
+	public boolean libReset() { return libReset(false); }
 
 	/**
 	 *	Reset the singleton and any other data caches after an application reload or other package or executable reloading event.
@@ -149,7 +162,7 @@ public final class MCFLibEntry implements IMCFLibEntry {
 	 *
 	 *	@return True if the library was reinitialized, false if the code detected that the library has not been used since the last reinitialization.
 	 */
-	public boolean libReset(boolean yesReally) { execMCFLibEntryLibReset(yesReally); }
+	public boolean libReset(boolean yesReally) { return execMCFLibEntryLibReset(yesReally); }
 
 	/**
 	 *	Reset the singleton and any data caches after an application reload or other package or executable reloading event.
@@ -158,7 +171,7 @@ public final class MCFLibEntry implements IMCFLibEntry {
 	 *
 	 *	@return	True if the singleton was reset, false if the singleton was set and yesReally was false.
 	 */
-	public boolean resetLib(boolean yesReally) { execMCFLibEntryLibReset(yesReally); }
+	public boolean resetLib(boolean yesReally) { return execMCFLibEntryLibReset(yesReally); }
 
 	/**
 	 *	Reset the singleton and any data caches after an application reload or other package or executable reloading event.
@@ -172,28 +185,28 @@ public final class MCFLibEntry implements IMCFLibEntry {
 	 *
 	 *	@return The parent's public resource name used for named resource resolution.
 	 */
-	public String getParentLibName() { return(PARENT_LIB_NAME); }
+	public String getParentLibName() { return(MCF_LIB_PARENT_NAME); }
 
 	/**
 	 *	Get name public resource name of this library or package, used in searches to resolve the package for runtimes or compilation and test.
 	 *
 	 *	@return The name of this package used for public naming resolution.
 	 */
-	public String getParentLibVersion() { return(PARENT_LIB_VERSION); }
+	public String getParentLibVersion() { return(MCF_LIB_PARENT_VERSION); }
 
 	/**
 	 *	Get the public resource name of this library or package, used in searches to resolve the package for runtimes or compilation and test.
 	 *
 	 *	@return The name of this package used for public naming resolution.
 	 */
-	public String getLibName() { return(LIB_NAME); }
+	public String getLibName() { return(MCF_LIB_NAME); }
 
 	/**
 	 *	Get the public resource name of this library or package, used in searches to resolve the package for runtimes or compilation and test.
 	 *
 	 *	@return The name of this package used for public naming resolution.
 	 */
-	public String getLibVersion() { return(LIB_VERSION); }
+	public String getLibVersion() { return(MCF_LIB_VERSION); }
 
 	/**
 	 *	The default main does nothing.
